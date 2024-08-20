7223435757b1ab9d4846c250895cae6e76048c15
Jakob Wolf <jakob-wolf14@live.dk>
2024-08-20 11:59:11 +0200

    bruh

---
+++ Lektion 1_6/MyList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lektion_1_6
+{
+    internal class MyList
+    {
+        public List<int> list = new List<int>();
+        public MyList() {}
+
+        public void Add(int number)
+        {
+            list.Add(number);
+        }
+        public void PrintNumbers()
+        {
+            int nmbr = 1;
+            foreach (int number in list)
+            {
+                Console.WriteLine("Tal nr " + nmbr + " i listen er " + number + ". :D");
+                nmbr++;
+            }
+        }
+    }
+}
---
+++ Lektion 1_6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lektion_1_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList list = new MyList();
+            list.Add(28);
+            list.Add(73);
+            list.Add(95);
+            list.Add(4);
+            list.Add(64);
+            list.Add(32);
+            list.Add(4);
+            list.PrintNumbers();
+            Console.ReadLine();
+        }
+    }
+}
